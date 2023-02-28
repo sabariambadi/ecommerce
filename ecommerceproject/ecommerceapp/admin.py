a6f5209ae4c5970f8afe7081e18a4187e94b8145
sabariambadi <sabarisab333@gmail.com>
2023-02-28 10:16:04 +0530

    'added'

---
+++ ecommerceproject/ecommerceapp/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . models import Category
+from  .models import Product
+
+# Register your models here.
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Category,categoryAdmin)
+
+class productAdmin(admin.ModelAdmin):
+    list_display = ['name','price','stock','available','created']
+    list_editable = ['price','stock','available']
+    prepopulated_fields = {'slug':('name',)}
+    list_per_page = 20
+admin.site.register(Product,productAdmin)
